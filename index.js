968816b1f01a65ad3264710ae1226025e6791963
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2025-05-22 20:09:50 -0300

    houve um erro,no entanto o erro foi sanado, devido o mesmo ter sido identificado, que foi o erro de grafia, foi digitado errado, e depois corrido, com isso os programas no postman deram certo e tudo rodou da maneira adequada

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const knex = require('./db');
 const app = express();
+app.use(express.json());
+knex.schema.hasTable('usuarios').then(exists => {
+  if (!exists) {
+    return knex.schema.createTable('usuarios', table => {
+      table.increments('id');
+      table.string('nome');
+      table.string('email');
+    });
+  }
+});
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/usuarios', async (req, res) => {
+  const usuarios = await knex('usuarios').select('*');
+    res.json(usuarios);
+});
 
-app.listen(3000, () => console.log('Servidor rodando'));
-
+app.post('/usuarios', async (req, res) => {
+  const { nome, email } = req.body;
+  await knex('usuarios').insert({ nome, email });
+  res.status(201).json({ mensagem: 'Usuário criado com sucesso' });
+});
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
